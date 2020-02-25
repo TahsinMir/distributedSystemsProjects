276151f8b5d3e92e5354c41fb0f308cf398e71f4
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-02-25 16:28:29 -0700

    Initally chatserver socket created

--- p1/src/ChatServer.java
+++ p1/src/ChatServer.java
@@ -1,5 +1,25 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.InetAddress;
 
 public class ChatServer {
+    ServerSocket ss;
+    int debug_level;
+
+    public ChatServer(int port, int debug_level) {
+        try {
+            ss = new ServerSocket(port);
+            System.out.println("ChatServer is up and running on port " + port + " " + InetAddress.getLocalHost());
+            this.debug_level = debug_level;
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
 
     public static void main(String args[]){
         if(args.length != 2){
@@ -10,7 +30,9 @@ public class ChatServer {
         // Debug level 0 is normal only error are reported
         // Debug level 1 shows all the events
         int debug_level = Integer.parseInt(args[2]);
-        //Create server socket
+        //Create chat server socket
+        ChatServer cs = new ChatServer(port, debug_level);
+
 
     }
 }
