05774ba26e15993a3f510ea2019e3f9b9e75bb4f
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-02-25 16:32:25 -0700

    Parameter udpated

--- p1/src/ChatServer.java
+++ p1/src/ChatServer.java
@@ -23,13 +23,13 @@ public class ChatServer {
 
     public static void main(String args[]){
         if(args.length != 2){
-            System.out.println("Usage: java ChatServer -p #port -d #debug_level");
+            System.out.println("Usage: java ChatServer #port #debug_level");
             System.exit(0);
         }
-        int port = Integer.parseInt(args[1]);
+        int port = Integer.parseInt(args[0]);
         // Debug level 0 is normal only error are reported
         // Debug level 1 shows all the events
-        int debug_level = Integer.parseInt(args[2]);
+        int debug_level = Integer.parseInt(args[1]);
         //Create chat server socket
         ChatServer cs = new ChatServer(port, debug_level);
 
