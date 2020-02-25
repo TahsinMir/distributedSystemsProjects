e047632a4cc0f802250c9fc1c8d36d77caaefeaa
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-02-25 16:18:46 -0700

    Taking arguments

--- p1/src/ChatServer.java
+++ p1/src/ChatServer.java
@@ -1,2 +1,16 @@
+
 public class ChatServer {
+
+    public static void main(String args[]){
+        if(args.length != 2){
+            System.out.println("Usage: java ChatServer -p #port -d #debug_level");
+            System.exit(0);
+        }
+        int port = Integer.parseInt(args[1]);
+        // Debug level 0 is normal only error are reported
+        // Debug level 1 shows all the events
+        int debug_level = Integer.parseInt(args[2]);
+        //Create server socket
+
+    }
 }
