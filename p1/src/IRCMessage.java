b7f87472f587b6df6a154b58328aa6aaf891593f
TahsinMir <tahsinimtiaz@u.boisestate.edu>
2020-02-25 16:55:25 -0700

    Common IRC message

---
+++ p1/src/IRCMessage.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+import java.util.Hashtable;
+import java.util.List;
+
+public class IRCMessage extends Object implements Serializable
+{
+	String serverName;
+	String nickName;
+	Hashtable<String, Integer> channelList;
+	String joiningChannel;
+	String leavingChannel;
+	boolean quit;
+	boolean help;
+	boolean stats;
+	
+	public IRCMessage()
+	{
+		channelList = new Hashtable<String, Integer>();
+	}
+}
+
