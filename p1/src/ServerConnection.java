95877f52e3328b0968f8b1697324bcd1688a9ae8
TahsinMir <tahsinimtiaz@u.boisestate.edu>
2020-03-02 01:50:59 -0700

    Removed codes during mergeconflict solving added again

--- p1/src/ServerConnection.java
+++ p1/src/ServerConnection.java
@@ -10,6 +10,9 @@ import java.io.ObjectInput;
 public class ServerConnection extends  Thread {
 
     private Socket client;
+    private Channel facebook;
+    private InputStream in;
+	private OutputStream out;
 
     ServerConnection(Socket client) throws SocketException
     {
@@ -21,43 +24,36 @@ public class ServerConnection extends  Thread {
         
         System.out.println("created channel");
     }
-
-    public void run(){
-        try {
     public void run()
     {
-        try 
-        {
-        	while(true)
+    	try
+    	{
+    		while(true)
         	{
         		in = client.getInputStream();
-				out = client.getOutputStream();
-				
-				//client goes first, and then goes the server, the one with the lower value wins
-				ObjectInputStream oin = new ObjectInputStream(in);
-				IRCMessage res = (IRCMessage) oin.readObject();
-				
-				System.out.println("received IRC message: ");
-				PrintIRCCommand(res);
-				
-				IRCMessage response = PrepareResponse(res);
-				
-				ObjectOutputStream oout = new ObjectOutputStream(out);
-				oout.writeObject(response);
-				oout.flush();
+    			out = client.getOutputStream();
+    			
+    			//client goes first, and then goes the server, the one with the lower value wins
+    			ObjectInputStream oin = new ObjectInputStream(in);
+    			IRCMessage res = (IRCMessage) oin.readObject();
+    			
+    			System.out.println("received IRC message: ");
+    			PrintIRCCommand(res);
+    			
+    			IRCMessage response = PrepareResponse(res);
+    			
+    			ObjectOutputStream oout = new ObjectOutputStream(out);
+    			oout.writeObject(response);
+    			oout.flush();
         	}
-    public void run(){
-        try {
-            // Variables to read and write object.
-            // These variables will be used to communicate with server.
-
-        }
-        catch (IOException e)
-        {
-            System.out.println("I/O error " + e);
-        } catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+    	}
+    	catch(IOException e)
+    	{
+    		System.out.println("IOException occured");
+    	}
+    	catch (ClassNotFoundException e)
+    	{
+    		System.out.println("ClassNotFoundException occured");
 		}
     }
     private IRCMessage PrepareResponse(IRCMessage command)
@@ -78,5 +74,26 @@ public class ServerConnection extends  Thread {
     	response.responseMessage = "order executed";
     	
     	return response;
+    }
+    private void PrintIRCCommand(IRCMessage message)
+    {
+        System.out.println("message.isCommand: " + message.isCommand);
+        System.out.println("message.isClientRequest: " + message.isClientRequest);
+        System.out.println("message.isServerResponse: " + message.isServerResponse);
+        
+        // all the IRC part
+        System.out.println("message.commandType: " + message.commandType);
+        System.out.println("message.serverName: " + message.serverName);
+        System.out.println("message.nickName: " + message.nickName);
+        System.out.println("message.channelList:" + message.channelList);
+        System.out.println("message.channelName: " + message.channelName);
+        System.out.println("message.leaveChannelType: " + message.leaveChannelType);
+        
+        //otherwise just a plain message
+        System.out.println("message.message: " + message.message);
+        
+        //no message could be created
+        System.out.println("message.error: " + message.error);
+        System.out.println("message.errorMessage: " + message.errorMessage);
     }
 }
