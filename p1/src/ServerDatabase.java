7df2d053691414788463c5d625a84d7da0f50545
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-03-03 16:04:32 -0700

    Serverdatabse modified

--- p1/src/ServerDatabase.java
+++ p1/src/ServerDatabase.java
@@ -11,34 +11,44 @@ public class ServerDatabase
 	
 	public ServerDatabase()
 	{
-		Users = new Hashtable<String, String>();
-		Channels = new Hashtable<String, Integer>();
-		threadIdList = new Hashtable<Long, Boolean>();
+		Users = new Hashtable<String, String>(); // username, channel name
+		Channels = new Hashtable<String, Integer>(); // Channel name, number of user in that channel
+		threadIdList = new Hashtable<Long, Boolean>(); // No work so far
 	}
-	
-	public void AddUser(String userName, String channelName)
-	{
-		Users.put(userName, channelName);
+
+	public boolean AddUserToChannel(String userName, String channelName){
+		if(Channels.containsKey(channelName)) {
+			int val = Channels.get(channelName);
+			Channels.replace(channelName, val + 1);
+			Users.put(userName, channelName);
+			return true;
+		}
+		return false; //Channel wasn't found so return True
 	}
-	public void AddChannel(String channelName)
+	public boolean CreateChannel(String channelName)
 	{
-		Channels.put(channelName, 0);
+		if(!Channels.containsKey(channelName)){
+			Channels.put(channelName, 0);
+			return true; // new channel created
+		}
+		return false; // Channel already exists
 	}
 	public void AddThread(long threadId)
 	{
 		threadIdList.put(threadId, true);
 	}
-	
-	public void AddUserToChannel(String channelName)
-	{
-		if(Channels.containsKey(channelName))
-		{
-			int val = Channels.get(channelName);
-			
-			Channels.replace(channelName, val + 1);
-		}
-	}
-	
+
+	//Removed everything into the addUser
+//	public void AddUserToChannel(String channelName)
+//	{
+//		if(Channels.containsKey(channelName))
+//		{
+//			int val = Channels.get(channelName);
+//
+//			Channels.replace(channelName, val + 1);
+//		}
+//	}
+	//No work so far
 	public boolean DoesThreadIdExist(long id)
 	{
 		if(threadIdList.containsKey(id))
